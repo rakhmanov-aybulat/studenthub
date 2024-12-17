8bcb68a132de6c539e56141fce8a959aa2306c25
Rakhmanov Aybulat <rakhmanov.aybulat@gmail.com>
2024-12-17 11:27:05 +0300

    feat: implement CRUD for all entities

--- src/main/java/org/studenthub/StudentHubController.java
+++ src/main/java/org/studenthub/StudentHubController.java
@@ -10,7 +10,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
-import org.studenthub.model.Student;
+import org.studenthub.model.*;
 
 public class StudentHubController implements Initializable {
     @FXML
@@ -25,6 +25,72 @@ public class StudentHubController implements Initializable {
     @FXML
     private TextField studentsStudentIdField;
 
+    @FXML
+    private TableView<Group> groupsTableView;
+
+    @FXML
+    private TextField groupsGroupNameField;
+
+    @FXML
+    private TextField groupsGroupIdField;
+
+    @FXML
+    private TableView<Discipline> disciplinesTableView;
+
+    @FXML
+    private TextField disciplinesDisciplineNameField;
+
+    @FXML
+    private TextField disciplinesDisciplineIdField;
+
+    @FXML
+    private TableView<Schedule> scheduleTableView;
+
+    @FXML
+    private TextField scheduleGroupIdField;
+
+    @FXML
+    private TextField scheduleDisciplineIdField;
+
+    @FXML
+    private TextField scheduleDateField;
+
+    @FXML
+    private TextField scheduleScheduleIdField;
+
+    @FXML
+    private TableView<Attendance> attendanceTableView;
+
+    @FXML
+    private TextField attendanceStudentIdField;
+
+    @FXML
+    private TextField attendanceScheduleIdField;
+
+    @FXML
+    private TextField attendancePresentField;
+
+    @FXML
+    private TextField attendanceAttendanceIdField;
+
+    @FXML
+    private TableView<PracticalWork> practicalWorksTableView;
+
+    @FXML
+    private TextField practicalWorksStudentIdField;
+
+    @FXML
+    private TextField practicalWorksDisciplineIdField;
+
+    @FXML
+    private TextField practicalWorksDateField;
+
+    @FXML
+    private TextField practicalWorksGradeField;
+
+    @FXML
+    private TextField practicalWorksPracticalWorkIdField;
+
     private StudentService studentService;
 
     public StudentHubController(StudentService studentService) {
@@ -33,11 +99,18 @@ public class StudentHubController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        initStudentsTableView();
+        setupStudentsTableView();
         updateStudentsTableView();
+        setupGroupsTableView();
+        updateGroupsTableView();
+
+        setupDisciplinesTableView();
+        setupScheduleTableView();
+        setupAttendanceTableView();
+        setupPracticalWorksTableView();
     }
 
-    private void initStudentsTableView() {
+    private void setupStudentsTableView() {
         TableColumn<Student, Integer> studentIdColumn = new TableColumn<>("Student ID");
         studentIdColumn.setCellValueFactory(new PropertyValueFactory<>("studentId"));
         studentsTableView.getColumns().add(studentIdColumn);
@@ -51,9 +124,112 @@ public class StudentHubController implements Initializable {
         studentsTableView.getColumns().add(groupIdColumn);
     }
 
+    private void setupGroupsTableView() {
+        TableColumn<Group, Integer> groupIdColumn = new TableColumn<>("Group ID");
+        groupIdColumn.setCellValueFactory(new PropertyValueFactory<>("groupId"));
+        groupsTableView.getColumns().add(groupIdColumn);
+
+        TableColumn<Group, String> groupNameColumn = new TableColumn<>("Group Name");
+        groupNameColumn.setCellValueFactory(new PropertyValueFactory<>("groupName"));
+        groupsTableView.getColumns().add(groupNameColumn);
+    }
+
+    private void setupDisciplinesTableView() {
+        TableColumn<Discipline, Integer> disciplineIdColumn = new TableColumn<>("Discipline ID");
+        disciplineIdColumn.setCellValueFactory(new PropertyValueFactory<>("disciplineId"));
+        disciplinesTableView.getColumns().add(disciplineIdColumn);
+
+        TableColumn<Discipline, String> disciplineNameColumn = new TableColumn<>("Discipline Name");
+        disciplineNameColumn.setCellValueFactory(new PropertyValueFactory<>("disciplineName"));
+        disciplinesTableView.getColumns().add(disciplineNameColumn);
+    }
+
+    private void setupScheduleTableView() {
+        TableColumn<Schedule, Integer> scheduleIdColumn = new TableColumn<>("Schedule ID");
+        scheduleIdColumn.setCellValueFactory(new PropertyValueFactory<>("scheduleId"));
+        scheduleTableView.getColumns().add(scheduleIdColumn);
+
+        TableColumn<Schedule, Integer> groupIdColumn = new TableColumn<>("Group ID");
+        groupIdColumn.setCellValueFactory(new PropertyValueFactory<>("groupId"));
+        scheduleTableView.getColumns().add(groupIdColumn);
+
+        TableColumn<Schedule, Integer> disciplineIdColumn = new TableColumn<>("Discipline ID");
+        disciplineIdColumn.setCellValueFactory(new PropertyValueFactory<>("disciplineId"));
+        scheduleTableView.getColumns().add(disciplineIdColumn);
+
+        TableColumn<Schedule, String> dateColumn = new TableColumn<>("Date");
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        scheduleTableView.getColumns().add(dateColumn);
+    }
+
+    private void setupAttendanceTableView() {
+        TableColumn<Attendance, Integer> attendanceIdColumn = new TableColumn<>("Attendance ID");
+        attendanceIdColumn.setCellValueFactory(new PropertyValueFactory<>("attendanceId"));
+        attendanceTableView.getColumns().add(attendanceIdColumn);
+
+        TableColumn<Attendance, Integer> studentIdColumn = new TableColumn<>("Student ID");
+        studentIdColumn.setCellValueFactory(new PropertyValueFactory<>("studentId"));
+        attendanceTableView.getColumns().add(studentIdColumn);
+
+        TableColumn<Attendance, Integer> scheduleIdColumn = new TableColumn<>("Schedule ID");
+        scheduleIdColumn.setCellValueFactory(new PropertyValueFactory<>("scheduleId"));
+        attendanceTableView.getColumns().add(scheduleIdColumn);
+
+        TableColumn<Attendance, Boolean> presentColumn = new TableColumn<>("Present");
+        presentColumn.setCellValueFactory(new PropertyValueFactory<>("present"));
+        attendanceTableView.getColumns().add(presentColumn);
+    }
+
+    private void setupPracticalWorksTableView() {
+        TableColumn<PracticalWork, Integer> practicalWorkIdColumn = new TableColumn<>("Practical Work ID");
+        practicalWorkIdColumn.setCellValueFactory(new PropertyValueFactory<>("practicalWorkId"));
+        practicalWorksTableView.getColumns().add(practicalWorkIdColumn);
+
+        TableColumn<PracticalWork, Integer> studentIdColumn = new TableColumn<>("Student ID");
+        studentIdColumn.setCellValueFactory(new PropertyValueFactory<>("studentId"));
+        practicalWorksTableView.getColumns().add(studentIdColumn);
+
+        TableColumn<PracticalWork, Integer> disciplineIdColumn = new TableColumn<>("Discipline ID");
+        disciplineIdColumn.setCellValueFactory(new PropertyValueFactory<>("disciplineId"));
+        practicalWorksTableView.getColumns().add(disciplineIdColumn);
+
+        TableColumn<PracticalWork, String> dateColumn = new TableColumn<>("Date");
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+        practicalWorksTableView.getColumns().add(dateColumn);
+
+        TableColumn<PracticalWork, Integer> gradeColumn = new TableColumn<>("Grade");
+        gradeColumn.setCellValueFactory(new PropertyValueFactory<>("grade"));
+        practicalWorksTableView.getColumns().add(gradeColumn);
+    }
+
     private void updateStudentsTableView() {
         ObservableList<Student> students = studentService.getStudentObservableList();
         studentsTableView.setItems(students);
+    }
+
+    private void updateGroupsTableView() {
+        ObservableList<Group> groups = studentService.getGroupsObservableList();
+        groupsTableView.setItems(groups);
+    }
+
+    public void updateDisciplinesTableView() {
+        ObservableList<Discipline> disciplines = studentService.getDisciplinesObservableList();
+        disciplinesTableView.setItems(disciplines);
+    }
+
+    public void updateScheduleTableView() {
+        ObservableList<Schedule> schedules = studentService.getSchedulesObservableList();
+        scheduleTableView.setItems(schedules);
+    }
+
+    public void updateAttendanceTableView() {
+        ObservableList<Attendance> attendances = studentService.getAttendancesObservableList();
+        attendanceTableView.setItems(attendances);
+    }
+
+    public void updatePracticalWorksTableView() {
+        ObservableList<PracticalWork> practicalWorks = studentService.getPracticalWorksObservableList();
+        practicalWorksTableView.setItems(practicalWorks);
     }
 
     @FXML
@@ -69,11 +245,130 @@ public class StudentHubController implements Initializable {
         studentsFullNameField.clear();
         studentsGroupIdField.clear();
     }
+
     @FXML
     private void handleRemoveStudentButtonClick() {
         int studentId = Integer.parseInt(studentsStudentIdField.getText());
         studentService.removeStudent(studentId);
         updateStudentsTableView();
+
         studentsStudentIdField.clear();
+    }
+
+    @FXML
+    private void handleAddGroupButtonClick() {
+        String groupName = groupsGroupNameField.getText();
+        Group group = new Group(-1, groupName);
+
+        studentService.addGroup(group);
+
+        updateGroupsTableView();
+        groupsGroupNameField.clear();
+    }
+
+    @FXML
+    private void handleRemoveGroupButtonClick() {
+        int groupId = Integer.parseInt(groupsGroupIdField.getText());
+        studentService.removeGroup(groupId);
+        updateGroupsTableView();
+
+        studentsGroupIdField.clear();
+    }
+
+    @FXML
+    private void handleAddDisciplineButtonClick() {
+        String disciplineName = disciplinesDisciplineNameField.getText();
+        Discipline discipline = new Discipline(-1, disciplineName);
+
+        studentService.addDiscipline(discipline);
+        updateDisciplinesTableView();
+
+        disciplinesDisciplineNameField.clear();
+    }
+
+    @FXML
+    private void handleRemoveDisciplineButtonClick() {
+        int disciplineId = Integer.parseInt(disciplinesDisciplineIdField.getText());
+        studentService.removeDiscipline(disciplineId);
+        updateDisciplinesTableView();
+
+        disciplinesDisciplineIdField.clear();
+    }
+
+    @FXML
+    private void handleAddScheduleButtonClick() {
+        int groupId = Integer.parseInt(scheduleGroupIdField.getText());
+        int disciplineId = Integer.parseInt(scheduleDisciplineIdField.getText());
+        String date = scheduleDateField.getText();
+
+        Schedule schedule = new Schedule(-1, groupId, disciplineId, date);
+
+        studentService.addSchedule(schedule);
+        updateScheduleTableView();
+
+        scheduleGroupIdField.clear();
+        scheduleDisciplineIdField.clear();
+        scheduleDateField.clear();
+    }
+
+    @FXML
+    private void handleRemoveScheduleButtonClick() {
+        int scheduleId = Integer.parseInt(scheduleGroupIdField.getText());
+        studentService.removeSchedule(scheduleId);
+        updateScheduleTableView();
+
+        scheduleGroupIdField.clear();
+    }
+
+    @FXML
+    private void handleAddAttendanceButtonClick() {
+        int studentId = Integer.parseInt(attendanceStudentIdField.getText());
+        int scheduleId = Integer.parseInt(attendanceScheduleIdField.getText());
+        boolean present = Boolean.parseBoolean(attendancePresentField.getText());
+
+        Attendance attendance = new Attendance(-1, studentId, scheduleId, present);
+
+        studentService.addAttendance(attendance);
+        updateAttendanceTableView();
+
+        attendanceStudentIdField.clear();
+        attendanceScheduleIdField.clear();
+        attendancePresentField.clear();
+    }
+
+    @FXML
+    private void handleRemoveAttendanceButtonClick() {
+        int attendanceId = Integer.parseInt(attendanceStudentIdField.getText());
+        studentService.removeAttendance(attendanceId);
+        updateAttendanceTableView();
+
+        attendanceStudentIdField.clear();
+    }
+
+    @FXML
+    private void handleAddPracticalWorkButtonClick() {
+        int studentId = Integer.parseInt(practicalWorksStudentIdField.getText());
+        int disciplineId = Integer.parseInt(practicalWorksDisciplineIdField.getText());
+        String date = practicalWorksDateField.getText();
+        int grade = Integer.parseInt(practicalWorksGradeField.getText());
+
+        PracticalWork practicalWork = new PracticalWork(-1, studentId, disciplineId, date, grade);
+
+        studentService.addPracticalWork(practicalWork);
+        updatePracticalWorksTableView();
+
+        practicalWorksStudentIdField.clear();
+        practicalWorksDisciplineIdField.clear();
+        practicalWorksDateField.clear();
+        practicalWorksGradeField.clear();
+    }
+
+    @FXML
+    private void handleRemovePracticalWorkButtonClick() {
+        int practicalWorkId = Integer.parseInt(practicalWorksStudentIdField.getText());
+        studentService.removePracticalWork(practicalWorkId);
+        updatePracticalWorksTableView();
+
+        practicalWorksStudentIdField.clear();
     }
 }
--- src/main/java/org/studenthub/StudentService.java
+++ src/main/java/org/studenthub/StudentService.java
@@ -2,7 +2,7 @@ package org.studenthub;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import org.studenthub.model.Student;
+import org.studenthub.model.*;
 
 import java.sql.ResultSet;
 import java.util.List;
@@ -45,5 +45,162 @@ public class StudentService {
             e.printStackTrace();
         }
         return students;
+    }
+
+    public ObservableList<Group> getGroupsObservableList() {
+        ObservableList<Group> groups = FXCollections.observableArrayList();
+        String query = "SELECT group_id, group_name FROM GROUPS";
+        try (ResultSet rs = db.executeQuery(query)) {
+            while (rs.next()) {
+                groups.add(new Group(
+                        rs.getInt("group_id"),
+                        rs.getString("group_name"))
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return groups;
+    }
+
+    public void addGroup(Group group) {
+        db.executePreparedUpdate(
+                "INSERT INTO GROUPS (group_name) VALUES (?)",
+                group.getGroupName());
+    }
+
+    public void removeGroup(int groupId) {
+        db.executePreparedUpdate(
+                "DELETE FROM GROUPS WHERE group_id=?", groupId);
+    }
+
+    public ObservableList<Discipline> getDisciplinesObservableList() {
+        ObservableList<Discipline> disciplines = FXCollections.observableArrayList();
+        String query = "SELECT discipline_id, discipline_name FROM DISCIPLINES";
+        try (ResultSet rs = db.executeQuery(query)) {
+            while (rs.next()) {
+                disciplines.add(new Discipline(
+                        rs.getInt("discipline_id"),
+                        rs.getString("discipline_name")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return disciplines;
+    }
+
+    public void addDiscipline(Discipline discipline) {
+        db.executePreparedUpdate(
+                "INSERT INTO DISCIPLINES (discipline_name) VALUES (?)",
+                discipline.getDisciplineName()
+        );
+    }
+
+    public void removeDiscipline(int disciplineId) {
+        db.executePreparedUpdate(
+                "DELETE FROM DISCIPLINES WHERE discipline_id=?",
+                disciplineId
+        );
+    }
+
+    public ObservableList<Schedule> getSchedulesObservableList() {
+        ObservableList<Schedule> schedules = FXCollections.observableArrayList();
+        String query = "SELECT schedule_id, group_id, " +
+                "discipline_id, date FROM SCHEDULE";
+        try (ResultSet rs = db.executeQuery(query)) {
+            while (rs.next()) {
+                schedules.add(new Schedule(
+                        rs.getInt("schedule_id"),
+                        rs.getInt("group_id"),
+                        rs.getInt("discipline_id"),
+                        rs.getString("date")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return schedules;
+    }
+
+    public void addSchedule(Schedule schedule) {
+        db.executePreparedUpdate(
+                "INSERT INTO SCHEDULE (group_id, discipline_id, date) " +
+                        "VALUES (?, ?, ?)", schedule.getGroupId(),
+                schedule.getDisciplineId(), schedule.getDate()
+        );
+    }
+
+    public void removeSchedule(int scheduleId) {
+        db.executePreparedUpdate(
+                "DELETE FROM SCHEDULE WHERE schedule_id=?", scheduleId);
+    }
+
+    public ObservableList<Attendance> getAttendancesObservableList() {
+        ObservableList<Attendance> attendances = FXCollections.observableArrayList();
+        String query = "SELECT attendance_id, student_id, " +
+                "schedule_id, present FROM ATTENDANCE";
+        try (ResultSet rs = db.executeQuery(query)) {
+            while (rs.next()) {
+                attendances.add(new Attendance(
+                        rs.getInt("attendance_id"),
+                        rs.getInt("student_id"),
+                        rs.getInt("schedule_id"),
+                        rs.getBoolean("present")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return attendances;
+    }
+
+    public void addAttendance(Attendance attendance) {
+        db.executePreparedUpdate(
+                "INSERT INTO ATTENDANCE (student_id, schedule_id, present) " +
+                        "VALUES (?, ?, ?)", attendance.getStudentId(),
+                attendance.getScheduleId(), attendance.isPresent());
+    }
+
+    public void removeAttendance(int attendanceId) {
+        db.executePreparedUpdate(
+                "DELETE FROM ATTENDANCE WHERE attendance_id=?",
+                attendanceId);
+    }
+
+    public ObservableList<PracticalWork> getPracticalWorksObservableList() {
+        ObservableList<PracticalWork> practicalWorks = FXCollections.observableArrayList();
+        String query = "SELECT practical_work_id, student_id, " +
+                "discipline_id, date, grade FROM PRACTICALWORKS";
+        try (ResultSet rs = db.executeQuery(query)) {
+            while (rs.next()) {
+                practicalWorks.add(new PracticalWork(
+                        rs.getInt("practical_work_id"),
+                        rs.getInt("student_id"),
+                        rs.getInt("discipline_id"),
+                        rs.getString("date"),
+                        rs.getInt("grade")
+                ));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return practicalWorks;
+    }
+
+    public void addPracticalWork(PracticalWork practicalWork) {
+        db.executePreparedUpdate(
+                "INSERT INTO PRACTICALWORKS (student_id, discipline_id, " +
+                        "date, grade) VALUES (?, ?, ?, ?)",
+                practicalWork.getStudentId(), practicalWork.getDisciplineId(),
+                practicalWork.getDate(), practicalWork.getGrade()
+        );
+    }
+
+    public void removePracticalWork(int practicalWorkId) {
+        db.executePreparedUpdate(
+                "DELETE FROM PRACTICALWORKS WHERE practical_work_id=?",
+                practicalWorkId
+        );
     }
 }
